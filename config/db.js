87e7e95a3261b3a1e291c9932ad32af62e119aab
fikrinuruliman19 <90210895+fikrinuruliman19@users.noreply.github.com>
2026-07-28 18:18:24 +0700

    db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try {
         // Ganti URL di bawah dengan Connection String dari MongoDB Atlas Anda
         // Contoh: const conn = await mongoose.connect('mongodb+srv://username:password@cluster.mongodb.net/transgadget?retryWrites=true&w=majority');
-        const conn = await mongoose.connect('mongodb://127.0.0.1:27017/transgadget');
+        const conn = await mongoose.connect('mongodb+srv://fikrinurul231_db_user:<db_password>@cluster0.782nsjw.mongodb.net/?appName=Cluster0');
         console.log(`MongoDB Terhubung: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
